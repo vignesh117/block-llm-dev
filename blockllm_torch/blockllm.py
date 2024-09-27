98b36f42b5fdb6399d0f55ce4dd0a82f4c45d24e
Vignesh <vignesh117@gmail.com>
2024-09-27 11:08:08 -0700

    Update blockllm.py

--- blockllm_torch/blockllm.py
+++ blockllm_torch/blockllm.py
@@ -1,12 +1,20 @@
+from collections import defaultdict
 import math
+from typing import List
 import torch
 from torch.optim.optimizer import Optimizer
+import ipdb
+from torch.optim import SparseAdam, Adam
+from dataclasses import dataclass
+from torch import Tensor
+import random
 
 
 class BlockLLM(Optimizer):
     def __init__(
         self,
-        params,
+        named_params,
+        # named_parameter_list=[],
         lr=1e-3,
         betas=(0.9, 0.999),
         eps=1e-8,
@@ -14,7 +22,8 @@ class BlockLLM(Optimizer):
         sparsity_level=0.9,
         update_freq=1000,
         param_names=None,
-        model=None,
+        num_bottom_to_sample=1,
+        # model=None,
     ):
         if not 0.0 <= sparsity_level < 1.0:
             raise ValueError(f"Invalid sparsity level: {sparsity_level}")
@@ -26,8 +35,25 @@ class BlockLLM(Optimizer):
             raise ValueError(f"Invalid beta parameter at index 0: {betas[0]}")
         if not 0.0 <= betas[1] < 1.0:
             raise ValueError(f"Invalid beta parameter at index 1: {betas[1]}")
-        if not model:
-            raise ValueError("Model is required")
+        # if not model:
+        #     raise ValueError("Model is required")
+        self.internal_optimizer = None  # will be set in reset_optimizer
+        optim_groups = []
+
+        # disable gradients for embedding and lm_head layers
+
+        for param_name, param in named_params:
+            # sparse_hook = self.sparse_update_hook(param_name)
+            # for param in params:
+            if ("embed" in param_name) or ("lm_head" in param_name):
+                param.requires_grad = False
+                continue
+            if not param.requires_grad:
+                continue
+            state = {}
+            state["name"] = param_name
+            state["params"] = param
+            optim_groups.append(state)
 
         defaults = dict(
             lr=lr,
@@ -36,187 +62,203 @@ class BlockLLM(Optimizer):
             weight_decay=weight_decay,
             sparsity_level=sparsity_level,
             update_freq=update_freq,
-            param_names=param_names,
-            model=model,
-            top_k_params=[],
-            num_params_to_keep=0,  # this is dynamic, selected after layers are selected
+            top_k_param_names=[],
+            bottom_k_param_names=None,
+            mask={},
+            grad_norm_dict={},
+            num_bottom_to_sample=num_bottom_to_sample,
         )
-        super(BlockLLM, self).__init__(params, defaults)
+        super(BlockLLM, self).__init__(optim_groups, defaults)
+        # super(BlockLLM, self).__init__(defaults)
 
         self.sparsity_level = sparsity_level
         self.update_freq = update_freq
-        self.param_names = param_names
-        self.param_visit_count = (
-            {name: 0 for name in param_names} if param_names else {}
-        )
+        # self.param_names = param_names
         self.total_params = sum(
             p.numel() for group in self.param_groups for p in group["params"]
         )
-        self.param_dist_weights = self._compute_param_dist_weights()
         self.update_step = 0
+        self.num_params_to_keep = int(
+            (1 - self.sparsity_level) * len(self.param_groups)
+        )
 
-    def _compute_param_dist_weights(self):
-        weights = {}
-        for i, group in enumerate(self.param_groups):
-            for j, p in enumerate(group["params"]):
-                name = (
-                    self.param_names[i * len(group["params"]) + j]
-                    if self.param_names
-                    else f"param_{i}_{j}"
-                )
-                weights[name] = (i * len(group["params"]) + j + 1) / len(
-                    self.param_groups
-                )
-        return weights
+        # Compute top_k_params based on the weight magnitude
+        self._choose_top_k_params_by_mag()
 
-    def _bookkeep_mask(self, curr_mask: torch.Tensor, p_name):
-        # if p_name not in self.updated_parameters:
-        if self.updated_parameters.get(p_name) is None:
-            self.updated_parameters[p_name] = curr_mask
+        # Make the requires_grad False for the remaining parameters
+        for group in self.param_groups:
+            name = group["name"]
+            for param in group["params"]:
+                if name not in self.defaults["top_k_param_names"]:
+                    param.requires_grad = False
+                    param.grad = None
 
+        self._update_bottom_k_params()
+
+    @torch.no_grad()
+    def _choose_top_k_params_by_mag(self) -> List[str]:
+        """
+        Choose the top k parameters based on the weight magnitude.
+        This function is intended to be used before the training starts.
+        """
+        mag_param_names_weighted = [
+            (group["name"], param.data.abs().norm().item(), param)
+            for group in self.param_groups
+            for param in group["params"]
+        ]
+        # sorted_param_names = [
+        #     (name, param)
+        #     for name, _, param in sorted(
+        #         mag_param_names_weighted,
+        #         key=lambda x: x[1],
+        #         reverse=True,
+        #     )
+        # ]
+        random.shuffle(mag_param_names_weighted)
+        sorted_param_names = [(x[0], x[2]) for x in mag_param_names_weighted]
+
+        top_k_param_names = []
+        top_k_params = []
+        for name, param in sorted_param_names:
+            if "embed" in name or "lm_head" in name:
+                continue
+            top_k_param_names.append(name)
+            top_k_params.append(param)
+            if len(top_k_params) >= self.num_params_to_keep:
+                break
+        # top_k_param_names = [
+        #     name for name, _ in sorted_param_names[: self.num_params_to_keep]
+        # ]
+        # top_k_params = [
+        #     param for _, param in sorted_param_names[: self.num_params_to_keep]
+        # ]
+
+        self.defaults["top_k_param_names"] = top_k_param_names
+        self.reset_optimizer(top_k_params, mode="dense")
+        del (
+            sorted_param_names,
+            mag_param_names_weighted,
+        )
+        torch.cuda.empty_cache()
+
+    def _update_bottom_k_params(self):
+        # Create a cycle for the bottom k parameters
+        bottom_k_params = [
+            param
+            for group in self.param_groups
+            for param in group["params"]
+            if param not in self.defaults["top_k_param_names"]
+        ]
+        # self.defaults["bottom_k_param_names"] = itertools.cycle(bottom_k_params)
+        self.defaults["bottom_k_param_names"] = bottom_k_params
+        # del bottom_k_params
+
+    def reset_optimizer(self, parameters, mode="sparse"):
+        if mode == "sparse":
+            self.internal_optimizer = SparseAdam(
+                parameters, lr=self.defaults["lr"], betas=self.defaults["betas"]
+            )
         else:
-            self.updated_parameters[p_name] = (
-                curr_mask.bool() | self.updated_parameters[p_name]
+            self.internal_optimizer = Adam(
+                parameters, lr=self.defaults["lr"], betas=self.defaults["betas"]
             )
 
-    def _compute_mask(self, pname, grad, quantile, k):
-        """Compute the mask by selecting the top k gradients."""
-        if pname in self.defaults["mask"]:
-            return self.defaults["mask"][pname]
-        else:
-            # Flatten the gradient tensor and get the absolute values
-            k = min(k, len(grad.view(-1)))
-            flat_grad = torch.abs(grad).flatten()
-            _, top_k_indices = torch.topk(flat_grad, k)
-            # Initialize a mask of zeros
-            mask = torch.zeros_like(flat_grad, dtype=torch.bool)
-
-            # Set the top k positions to 1
-            mask[top_k_indices] = 1
-            # Reshape the mask to the original gradient shape and convert to sparse
-            mask = mask.view_as(grad)
-            self.defaults["mask"][pname] = mask
-            return mask
-
-    # def _compute_mask(self, grad, k):
-    #     k = min(k, grad.numel())
-    #     flat_grad = torch.abs(grad).flatten()
-    #     _, top_k_indices = torch.topk(flat_grad, k)
-    #     mask = torch.zeros_like(flat_grad, dtype=torch.bool)
-    #     mask[top_k_indices] = 1
-    #     return mask.view_as(grad)
-
+    @torch.no_grad()
     def step(self, closure=None):
         loss = None
-        if closure is not None:
-            loss = closure()
-
-        if self.update_step % self.update_freq == 0:
+        if (self.update_step % self.update_freq == 0) and (self.update_step > 0):
             self._adjust_parameters()
+            # self.update_step += 1
+            # self._reset_state_dict()
+
+        # if self.update_step == 1:
+        #     self._adjust_parameters()
 
         self.update_step += 1
-        k_remaining = self.defaults["num_params_to_keep"]
+        self.internal_optimizer.step(closure)
 
+        # Sample bottom k parameters and make them trainable
+
+        # Update the gradient norms of all the trainable parameters
+        # in the grad_norm_dict
         for group in self.param_groups:
-            for pname, p in self.defaults["model"].named_parameters():
-                if p.grad is None:
+            for param in group["params"]:
+                if not param.requires_grad:
                     continue
-                if pname not in self.defaults.get("top_k_params", []):
-                    continue
-                grad = p.grad.data
-                if grad.is_sparse:
-                    raise RuntimeError("Adam does not support sparse gradients")
+                name = group["name"]
+                self.defaults["grad_norm_dict"][name] = param.grad.abs().norm().item()
 
-                state = self.state[p]
+                if self.update_step % 10 == 0:
+                    if name not in self.defaults["top_k_param_names"]:
+                        param.requires_grad = False
+                        param.grad = None
+        torch.cuda.empty_cache()
 
-                # State initialization
-                if len(state) == 0:
-                    state["step"] = 0
-                    state["exp_avg"] = torch.zeros_like(p.data)
-                    state["exp_avg_sq"] = torch.zeros_like(p.data)
+        # Make the bottom k parameters not trainable
 
-                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
-                beta1, beta2 = group["betas"]
-
-                state["step"] += 1
-
-                if group["weight_decay"] != 0:
-                    grad = grad.add(p.data, alpha=group["weight_decay"])
-
-                # Decay the first and second moment running average coefficient
-                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
-                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
-
-                denom = exp_avg_sq.sqrt().add_(group["eps"])
-
-                step_size = (
-                    group["lr"]
-                    * math.sqrt(1 - beta2 ** state["step"])
-                    / (1 - beta1 ** state["step"])
+        if self.update_step % 10 == 0:
+            for _ in range(self.defaults["num_bottom_to_sample"]):
+                random.choice(self.defaults["bottom_k_param_names"]).requires_grad = (
+                    True
                 )
-                k_remaining -= len((exp_avg / denom).view(-1))
-
-                # if self.update_step % group["update_freq"] == 0:
-                #     # Compute mask
-                #     mask = self._compute_mask(pname, exp_avg / denom, k_remaining)
-                #     k_remaining -= mask.sum().item()
-                #     state["mask"] = mask
-                # else:
-                #     mask = state.get("mask", torch.ones_like(p.data, dtype=torch.bool))
-
-                # self._bookkeep_mask(mask, pname)
-
-                mask = state.get("mask", torch.ones_like(p.data, dtype=torch.bool))
-
-                p.data.addcdiv_(exp_avg, denom, value=-step_size)
-                p.data.masked_fill_(~mask, 0)
-
+                # next(self.defaults["bottom_k_param_names"]).requires_grad = True
+                # bottom_param = (self.defaults["bottom_k_param_names"])
+                # bottom_param.requires_grad = True
         return loss
 
     def _adjust_parameters(self):
         """
-        Adjust parameters based on gradients magnitude and visit count.
-        This function is called every update_freq steps.
-        It selects the top k parameters based on gradients magnitude and visit count.
-        It then updates the visit count and the defaults with the top k parameters.
+        Find the top k parameters based on the gradient norms in the grad_norm_dict
+        Update the bottom k parameters iterable
         """
-        print("Adjusting parameters")
-        grad_mag_param_names_weighted = [
-            (name, param.grad.abs().norm().item())
-            for name, param in self.defaults["model"].named_parameters()
-            if param.grad is not None
-        ]
+        self.internal_optimizer = None
 
-        # Sort parameter names based on gradients magnitude and visit count
-        sorted_param_names = [
+        # Find the top k parameters based on the gradient norms
+        sorted_grad_norms = [
             name
             for name, _ in sorted(
-                grad_mag_param_names_weighted,
-                key=lambda x: x[1] / (self.param_visit_count.get(x[0], 1) + 1e-8),
+                self.defaults["grad_norm_dict"].items(),
+                key=lambda x: x[1],
                 reverse=True,
             )
         ]
-        num_params_to_keep = int((1 - self.sparsity_level) * self.total_params)
 
+        name_to_param = {group["name"]: group["params"] for group in self.param_groups}
+
+        top_k_param_names = []
         top_k_params = []
         running_param_count = 0
-        for name in sorted_param_names:
-            param = self._get_parameter_by_name(self.defaults["model"], name)
-            if running_param_count < num_params_to_keep:
-                top_k_params.append(name)
-                running_param_count += param.numel()
-            else:
+        num_params_to_keep = int((1 - self.sparsity_level) * self.total_params)
+
+        for name in sorted_grad_norms:
+            if "embed" in name or "lm_head" in name:
+                continue
+            running_param_count += len(name_to_param[name])
+            if running_param_count >= num_params_to_keep:
                 break
+            top_k_param_names.append(name)
+            top_k_params.extend(name_to_param[name])
 
-        # Update visit count
-        for name in top_k_params:
-            self.param_visit_count[name] = self.param_visit_count.get(name, 0) + 1
+        # Update the top k parameters
+        self.reset_optimizer(top_k_params, mode="dense")
 
-        # Update defaults with top_k_params
-        self.defaults["top_k_params"] = top_k_params
+        # Make the requires_grad False for the remaining parameters
+        for group in self.param_groups:
+            name = group["name"]
+            for param in group["params"]:
+                if name not in top_k_param_names:
+                    param.requires_grad = False
+                    param.grad = None
 
-    def _get_parameter_by_name(self, model, name):
-        for param_name, param in model.named_parameters():
-            if param_name == name:
-                return param
-        return None
+        # clear the gradient norms
+        self.defaults["grad_norm_dict"] = {}
+
+        # Update the bottom k parameters iterable
+        self._update_bottom_k_params()
+        del (
+            top_k_params,
+            sorted_grad_norms,
+            top_k_param_names,
+            name_to_param,
+        )
+        torch.cuda.empty_cache()
